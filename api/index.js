e649e51b190ec75bba73fd8f2be38c2bd9473a31
sparshak7 <riley.onduty@gmail.com>
2023-10-09 22:04:16 +0530

    Added password hash and data to mongo

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose.connect(
@@ -14,9 +15,11 @@ mongoose.connect(
 
 const app=express();
 const port=3000;
+app.use(express.json());
 
 app.listen(port, ()=>{
   console.log(`Server running at port ${port}`);
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
