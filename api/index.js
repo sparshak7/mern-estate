dbc9a09ddbcc5cdded35e677745142d6dfbd46bf
sparshak7 <riley.onduty@gmail.com>
2023-10-13 14:08:12 +0530

    created api listing route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose.connect(
@@ -18,6 +19,7 @@ const app=express();
 const port=3000;
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/listing", listingRouter);
 
 app.listen(port, ()=>{
   console.log(`Server running at port ${port}`);
