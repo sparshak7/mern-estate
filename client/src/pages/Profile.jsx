1175d6a15b5f5e2e9b8fccbb8c43ffd48f3ba841
sparshak7 <riley.onduty@gmail.com>
2023-10-16 12:49:53 +0530

    added show listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,34 +1,50 @@
-import { useSelector } from "react-redux"
-import { useRef,useState,useEffect } from "react";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess } from "../redux/user/userSlice";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
 
 const Profile = () => {
-  const {currentUser, loading, error} = useSelector(state => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
-  const [file,setFile] = useState(undefined);
+  const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [success, setSuccess] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
 
-  useEffect(()=>{
-    if(file){
+  useEffect(() => {
+    if (file) {
       handleFileUpload(file);
     }
   }, [file]);
 
-  const handleFileUpload = (file)=>{
+  const handleFileUpload = (file) => {
     const storage = getStorage(app);
     const fileName = new Date().getTime() + file.name;
-    const storageRef = ref(storage,fileName);
+    const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
     uploadTask.on(
       "state_changed",
@@ -52,25 +68,25 @@ const Profile = () => {
         });
       }
     );
-  }
+  };
 
-  const handleChange = (e)=>{
-    setFormData({...formData, [e.target.id]: e.target.value})
-  }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
-      })
-      const data = await res.json()
-      if(data.success === false){
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
         dispatch(updateUserFailure(data.message));
         enqueueSnackbar("There was an error updating the user. Try again.", {
           variant: "error",
@@ -86,16 +102,16 @@ const Profile = () => {
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
-  }
+  };
 
-  const handleDelete = async()=>{
+  const handleDelete = async () => {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`api/user/delete/${currentUser._id}`, {
-        method: 'DELETE',
-      })
+        method: "DELETE",
+      });
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
         enqueueSnackbar("There was an error deleting the user. Try again.", {
           variant: "error",
@@ -109,18 +125,20 @@ const Profile = () => {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
 
-  const handleSignOut = async()=>{
+  const handleSignOut = async () => {
     try {
       dispatch(signOutUserStart());
-      const res = await fetch('/api/auth/signout', {
+      const res = await fetch("/api/auth/signout", {
         method: "GET",
       });
       const data = await res.json();
       if (data.success === false) {
         dispatch(signOutUserFailure(data.message));
-        enqueueSnackbar('There was an error signing out. Try again.', {variant: 'error'});
+        enqueueSnackbar("There was an error signing out. Try again.", {
+          variant: "error",
+        });
         return;
       }
       dispatch(signOutUserSuccess());
@@ -130,7 +148,29 @@ const Profile = () => {
     } catch (error) {
       dispatch(signOutUserFailure(error.message));
     }
-  }
+  };
+
+  const handleListing = async () => {
+    try {
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.success === false){
+        enqueueSnackbar("Error showing listings.", {
+          variant: "error",
+        });
+        return;
+      }
+      setUserListings(data);
+      enqueueSnackbar("Succesfully displayed. Scroll down.", {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar("Error showing listings.", {
+        variant: "error",
+      });
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-center text-3xl font-semibold my-7 dark:text-white">
@@ -209,11 +249,46 @@ const Profile = () => {
       </div>
       {/* <p className="text-red-700 mt-5 text-center">{error ? error : ""}</p>
       <p className="text-green-700 mt-5 text-center">{success ? 'Updated succesfully.' : ""}</p> */}
+      <button onClick={handleListing} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold dark:text-white">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p className="dark:text-white">{listing.title}</p>
+              </Link>
+
+              <div className="flex flex-col item-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
 
 //firebase image upload rules
 //  allow read;
